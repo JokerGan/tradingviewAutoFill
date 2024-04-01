f44d894364d1cad71aca881acfdc55fe6ad1274e
JokerGan <40454503+JokerGan@users.noreply.github.com>
2024-04-01 11:31:55 +0800

    Update tradingviewAutoFill.js
    增加做空填写；
    修复错误注释。

--- tradingviewAutoFill.js
+++ tradingviewAutoFill.js
@@ -12,6 +12,7 @@
 
 var reactFiber='';
 
+
 //获取React的Fiber
 function getReactFiber(){
   let reactFiber_dom = document.querySelector(".content-tBgV1m0B");
@@ -27,35 +28,57 @@ function getReactFiber(){
 window.onkeydown = function (event) {
   if(event.altKey && event.keyCode == 90){
     getReactFiber();
+    let ticks = '';
+    // 获取收盘价div
+    let close = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[5]/div[2]",document.body).iterateNext();
     // 获取最低价div
     let low = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/div[2]",document.body).iterateNext();
     // 获取最高价div
     let high = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[3]/div[2]",document.body).iterateNext();
     // 获取做多的ATR计算后的div
     let lowPlusAtr = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div",document.body).iterateNext();
+    // 获取空多的ATR计算后的div
+    let highMinusAtr = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/div[2]/div",document.body).iterateNext();
     // 获取3.6倍ATR
     let Atr = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/div[2]/div",document.body).iterateNext();
     // 获取当前光标所在元素
     let activeElement = document.activeElement;
 
+
     switch(activeElement.getAttribute('data-property-id')) {
       case 'Risk/RewardlongEntryPrice':
-        // 获取收盘价
-        let close = document.evaluate("/html/body/div[2]/div[5]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[5]/div[2]",document.body).iterateNext();
+       // 设置开仓价
+        reactFiber.memoizedProps.children[0].props.definition.definitions._value[3].properties.value.setValue(parseFloat(close.innerHTML));
+        break;
+      case 'Risk/RewardshortEntryPrice':
         // 设置开仓价
         reactFiber.memoizedProps.children[0].props.definition.definitions._value[3].properties.value.setValue(parseFloat(close.innerHTML));
         break;
+
       case 'Risk/RewardlongStopLevelPrice':
         // 设置止损价
         reactFiber.memoizedProps.children[2].props.definition.definitions._value[1].properties.value.setValue(parseFloat(low.innerHTML));
         // 让1个点
-        let ticks = reactFiber.memoizedProps.children[2].props.definition.definitions._value[0].properties.value.value();
+        ticks = reactFiber.memoizedProps.children[2].props.definition.definitions._value[0].properties.value.value();
         reactFiber.memoizedProps.children[2].props.definition.definitions._value[0].properties.value.setValue(ticks+1);
         break;
-      case 'Risk/RewardlongProfitLevelPrice':
+      case 'Risk/RewardshortStopLevelPrice':
         // 设置止损价
+        reactFiber.memoizedProps.children[2].props.definition.definitions._value[1].properties.value.setValue(parseFloat(high.innerHTML));
+        // 让1个点
+        ticks = reactFiber.memoizedProps.children[2].props.definition.definitions._value[0].properties.value.value();
+        reactFiber.memoizedProps.children[2].props.definition.definitions._value[0].properties.value.setValue(ticks+1);
+        break;
+
+      case 'Risk/RewardlongProfitLevelPrice':
+        // 设置止盈价
         reactFiber.memoizedProps.children[1].props.definition.definitions._value[1].properties.value.setValue(parseFloat(lowPlusAtr.innerHTML));
         break;
+      case 'Risk/RewardshortProfitLevelPrice':
+        // 设置止盈价
+        reactFiber.memoizedProps.children[1].props.definition.definitions._value[1].properties.value.setValue(parseFloat(highMinusAtr.innerHTML));
+        break;
+
       default:
         break;
     }
